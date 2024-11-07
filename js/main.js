f6a83b536a04206e926925846317f6d1e37033b9
donatimar <donatimar@gmail.com>
2024-11-07 12:59:11 +0100

    added API for img and title plus responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
+  .then((response) => response.json())
+  .then((post) => {
+    const photoContainer = document.getElementById("photoContainer");
 
+    post.forEach((photo) => {
+      const col = document.createElement("div");
+      col.className = "col-12 col-md-6 col-lg-4";
+
+      const photoDiv = document.createElement("div");
+      photoDiv.className = "border text-center";
+
+      // Img
+      const img = document.createElement("img");
+      img.src = photo.url;
+      img.alt = photo.title;
+      img.className = "img-fluid";
+
+      // Img Title
+      const title = document.createElement("p");
+      title.className = "mt-3";
+      title.textContent = photo.title;
+
+      // Aggiung img e title
+      photoDiv.appendChild(img);
+      photoDiv.appendChild(title);
+      col.appendChild(photoDiv);
+      photoContainer.appendChild(col);
+    });
+  })
+  .catch((error) =>
+    console.error("Errore durante il caricamento delle immagini:", error)
+  );
